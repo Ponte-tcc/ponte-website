fc71c1c7c032251455ebbb4ba05e173c55f18ee0
Nico <nico.vnx07@gmail.com>
2021-11-22 16:02:17 -0300

    nsei

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-const express = require("express")
-const server = express()
-const routes = require("./src/routes")
+const express = require("express");
+const server = express();
+const routes = require("./src/routes");
 
-const uri = "mongodb+srv://nico:123321@cluster0.rtak1.mongodb.net/pontewebsiteDB?retryWrites=true&w=majority"
-
-
+const uri =
+  "mongodb+srv://nico:123321@cluster0.rtak1.mongodb.net/pontewebsiteDB?retryWrites=true&w=majority";
 
 //middleware
 server.use((req, res, next) => {
-    //req.locals.success_msg = req.flash("success_msg")
-    //req.locals.error_msg = req.flash("error_msg")
-    next()
-    
-    })
-
+  //req.locals.success_msg = req.flash("success_msg")
+  //req.locals.error_msg = req.flash("error_msg")
+  next();
+});
 
 //usando template engine
-server.set("view engine", "ejs")
+server.set("view engine", "ejs");
 
 //conexão mongoose
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise
-mongoose.connect(uri, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-useFindAndModify: true,
-}).then(() => {
-    console.log("MongoDB Conectado...")
-}).catch((erro) => {
-    console.log("Houve um erro ao se conectar: " + erro)
-})
-
-
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    console.log("MongoDB Conectado...");
+  })
+  .catch((erro) => {
+    console.log("Houve um erro ao se conectar: " + erro);
+  });
 
 //habilitar arquivos statics
-server.use(express.static("public"))
-
+server.use(express.static("public"));
 
 //usar o req.body
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({ extended: true }));
 
-const session = require("express-session")
+const session = require("express-session");
 //sessions
-server.use(session({
+server.use(
+  session({
     secret: "asjdnIAOJSNDOjansd1238192",
     resave: true,
-    saveUninitialized: true
-}))
+    saveUninitialized: true,
+  })
+);
 
 //routes
-server.use(routes)
-server.listen(3000, () => console.log("Server Rodando!"))
+server.use(routes);
+server.listen(3000, () => console.log("Server Rodando!"));
