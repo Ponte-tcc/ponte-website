96747e652b03a926408429a04766404f5f717cf0
Nico <nico.vnx07@gmail.com>
2021-12-01 10:09:43 -0300

    openmodal

--- public/script/coment.js
+++ public/script/coment.js
@@ -1,6 +1,6 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+var openModalButtons = document.querySelectorAll('[data-modal-target]')
+var closeModalButtons = document.querySelectorAll('[data-close-button]')
+var overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
